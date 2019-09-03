c8078935610c37cc1b65d071f4adb0c52ca0dbe1
Broccolito <wanjungu@yahoo.com>
2019-09-03 14:46:11 -0700

    Auto Commit at Tue 09/03/2019 14:46:11.85

--- sync.bat
+++ sync.bat
@@ -1,7 +1,5 @@
-@echo off
-set /p msg = "Commit Message: "
 git add .
 git pull origin master
-git commit -m "%msg% %date% %time%"
+git commit -m "Auto Commit at %date% %time%"
 git push origin master
 
